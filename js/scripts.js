c47d2370584f1e2d328714334358e20b13f93778
daniel owen <dowen35@gmail.com>
2016-07-28 15:06:04 -0400

    added cards, rules, betting, bank

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 // to do: center message box, set background to green
-// after deal button has been clicked, disable it until reset button is pressed
-// fix case where player hits and
+// fix case where player hits and checkWin runs too early
+// fix case where player can be dealt a hand w/o betting
+// have bank update on reset
+// fix case where player can bet more than they have
 
 // DONE ---- set messages after game over
 // the table/game looks like rob made it; change this
 // DONE ---- what about those stupid 11, 12, 13?
-// DONE ---- what about aces?
-// DONE ---- the player can hit forever
+// what about aces? ------- FIX THIS
+// the player can hit forever
 // there is no win counter/bet system
 // DONE ---- there is no deck to draw from
 // the cards aren't red or black like they could or should be
@@ -20,45 +22,68 @@ var theDeck = [];
 var playersHand = [];
 var dealersHand = [];
 var topOfTheDeck = 5;
+var bank = 100;
+var bet = 0;
+
 
 $(document).ready(function(){
 
 	$('.deal-button').click(function(){
+		$('.deal-button').prop('disabled', true); // disable deal button
+		$('.five-button, .ten-button, .twentyFive-button, .fifty-button').prop('disabled', true); // disable betting buttons
 		createDeck(); // run a function that creates an array of 1h - 13c
-		shuffleDeck();
+		shuffleDeck(); // shuffle the deck
 
 		// push the new card onto the player's hand array; then place it on the DOM
 		playersHand.push(theDeck[0]);
-		placeCard('player', 'one', theDeck[0]);
-
+		setTimeout(function(){
+			placeCard('player', 'one', theDeck[0]);
+		}, 250);
+	
+		// push the new card onto the dealers's hand array; then place it on the DOM
 		dealersHand.push(theDeck[1]);
-		placeCard('dealer', 'one', theDeck[1]);
+		setTimeout(function(){
+			placeCard('dealer', 'one', theDeck[1]);
+		}, 500);
 
+		// push the new card onto the player's hand array; then place it on the DOM
 		playersHand.push(theDeck[2]);
-		placeCard('player', 'two', theDeck[2]);
+		setTimeout(function(){
+			placeCard('player', 'two', theDeck[2]);
+		}, 750);
 
+		// push the new card onto the dealers's hand array; then place it on the DOM
 		dealersHand.push(theDeck[3]);
-		placeCard('dealer', 'two', theDeck[3]);
+		setTimeout(function(){
+			placeCard('dealer', 'two', theDeck[3]);
+		}, 1000);
 
-		calculateTotal(playersHand, 'player');
-		calculateTotal(dealersHand, 'dealer');
+		calculateTotal(playersHand, 'player'); // calculate total for player
+		calculateTotal(dealersHand, 'dealer'); // calculate total for dealer
 	});
 
 	$('.hit-button').click(function(){
+		var playerTotal = calculateTotal(playersHand, 'player');
+		if (playerTotal < 21){
 		var slotForNewCard = '';
-		if (playersHand.length == 2){slotForNewCard = "three";}
-		else if(playersHand.length == 3){slotForNewCard = "four";}
-		else if(playersHand.length == 4){slotForNewCard = "five";}
-		else if(playersHand.length == 5){slotForNewCard = "six";}
-		placeCard('player', slotForNewCard, theDeck[topOfTheDeck]);
-		playersHand.push(theDeck[topOfTheDeck]);
-		calculateTotal(playersHand, 'player');
-		topOfTheDeck++;
-		checkWin();
+			if (playersHand.length == 2){slotForNewCard = "three";}
+			else if(playersHand.length == 3){slotForNewCard = "four";}
+			else if(playersHand.length == 4){slotForNewCard = "five";}
+			else if(playersHand.length == 5){slotForNewCard = "six";}
+
+			setTimeout(function(){
+				placeCard('player', slotForNewCard, theDeck[topOfTheDeck]);
+				playersHand.push(theDeck[topOfTheDeck]);
+				playerTotal = calculateTotal(playersHand, 'player');
+				topOfTheDeck++;
+			}, 250);
+		}
 	});
 
 	$('.stand-button').click(function(){
-		// player clicked on stand. what happends to the player? nothing.
+		// player clicked on stand. what happens to the player? loses ability to hit, bet
+		$('.hit-button').prop('disabled', true); // disable hit button
+		$('.five-button, .ten-button, .twentyFive-button, .fifty-button').prop('disabled', true); // disable betting buttons
 		var slotForNewCard = '';
 		var dealerTotal = calculateTotal(dealersHand, 'dealer');
 		while(dealerTotal < 17){
@@ -77,15 +102,24 @@ $(document).ready(function(){
 	});
 
 	$('.reset-button').click(function(){
-		playersHand = []; // empty the players hand
-		dealersHand = []; // empty the dealers hand
+		playersHand = []; // empty the player's hand
+		dealersHand = []; // empty the dealer's hand
 		theDeck = []; // empty the deck
 		topOfTheDeck = 5; // reset top of deck to 5
-		$('.message-center').text(''); // reset win/loss/push message to an empty string
+		$('.message-center').text(''); // reset win/loss/push message to empty string
 		$('.card').text(''); // reset cards to blank
 		$('.player-total-number').text('0'); // reset number in player total html
 		$('.dealer-total-number').text('0'); // reset number in dealer total html
-		disableButtons(false);
+		// $('.bet-total').text('0'); // reset number in bet total html
+		disableButtons(false); // re-enable all buttons for a new game
+		$('.five-button, .ten-button, .twentyFive-button, .fifty-button').prop('disabled', false); // enable betting buttons
+	});
+
+	$('.five-button, .ten-button, .twentyFive-button, .fifty-button').click(function(){
+		bank -= Number($(this).val()); // decrease bank by value of button
+		$('.bank-total').text(bank); // update html of bank
+		bet += Number($(this).val()); // increase bank by value of button
+		$('.bet-total').text(bet); // update html of bank
 	});
 
 });
@@ -95,41 +129,44 @@ function checkWin(){
 	var playerTotal = calculateTotal(playersHand, 'player');
 	// get dealer total
 	var dealerTotal = calculateTotal(dealersHand, 'dealer');
+	// assign player outcome
+	var playerOutcome = '';
 
 	if (playerTotal > 21){ // player has busted
-		// set a message somewhere that says this
+		playerOutcome = 'lose';
 		$('.message-center').text('You busted. Dealer Wins!');
 	}else if(dealerTotal > 21){ // dealer has busted
-		// set a message somewhere that says this
+		playerOutcome = 'win';
 		$('.message-center').text('Dealer busted. You Win!');
 	}else{ // neither player has more than 21
-		if (playerTotal > dealerTotal){
-			// player won. say this somewhere
+		if((playerTotal == 21) && (playersHand.length == 2) && (playerTotal > dealerTotal)){ // player has blackjack; dealer does not
+			playerOutcome = 'blackjack'
+			$('.message-center').text('Blackjack! You Win!');
+		}else if(playerTotal > dealerTotal){ // player has more than dealer
+			playerOutcome = 'win';
 			$('.message-center').text('You Win!');
-		}else if (dealerTotal > playerTotal){
-			// dealer won. say this somewhere
+		}else if(dealerTotal > playerTotal){ // dealer has more than player
+			playerOutcome = 'lose';
 			$('.message-center').text('Dealer Wins!');
 		}else{
-			// push (tie). say this somewhere
+			playerOutcome = 'push';
 			$('.message-center').text('Push');
 		}
 	}
 	disableButtons(true);
+	payout(playerOutcome, bet);
 }
 
+////////////////////////////////////////////
 function placeCard(who, where, cardToPlace){
 	var classSelector = '.'+who+'-cards .card-'+where;
-
-	// write logic to fix the 11, 12, 13 issue
-
-	$(classSelector).html(cardToPlace);
+	$(classSelector).html('<img src="images/'+cardToPlace+'.png">');
 }
+////////////////////////////////////////////
 
+////////////////////////////////////////////
 function createDeck(){
-// fill the deck with 
-// - 52 cards
-// - 4 suits
-// 	- h, s, d, c
+// fill the deck with: 52 cards; 13 cards for each of the 4 suits (h, s, d, c)
 	var suits = ['h', 's', 'd', 'c'];
 	for(var s = 0; s < suits.length; s++){
 		for (var c = 1; c <= 13; c++){
@@ -137,15 +174,11 @@ function createDeck(){
 		}
 	}
 }
+////////////////////////////////////////////
 
+////////////////////////////////////////////
 function shuffleDeck(){
-// [1]
-// [2]
-// [3]
-// ...
-// [50]
-// [51]
-// [52]
+// [1], [2], [3],...[50], [51], [52]
 	for(var i = 0; i < 1000; i++){
 		card1 = Math.floor(Math.random() * theDeck.length);
 		card2 = Math.floor(Math.random() * theDeck.length);
@@ -154,20 +187,23 @@ function shuffleDeck(){
 		theDeck[card2] = temp;
 	}
 }
+////////////////////////////////////////////
 
+////////////////////////////////////////////
 function calculateTotal(hand, whosTurn){
-	// console.log(hand);
-	// console.log(whosTurn);
+	var hasAce = false;
 	var cardValue = 0;
 	var total = 0;
 	for (var i = 0; i < hand.length; i++){
-		cardValue = Number(hand[i].slice(0,-1));	
-		if (cardValue > 10){
-			cardValue = 10;
-		}else if(cardValue == 1 && total < 11){
+		cardValue = Number(hand[i].slice(0,-1));
+		if ((cardValue == 1) && ((total + 11) <= 21)){
 			cardValue = 11;
-		}else if(cardValue == 1 && total > 10){
-			cardValue = 1;
+			hasAce = true;
+		}else if (cardValue > 10){
+			cardValue = 10;
+		}else if ((cardValue + total > 21) && (hasAce)){
+			total = total - 10;
+			hasAce = false;
 		}
 		total += cardValue;
 	}
@@ -176,9 +212,25 @@ function calculateTotal(hand, whosTurn){
 	$(elementToUpdate).text(total);
 	return total;
 }
+////////////////////////////////////////////
 
-function disableButtons(TF){
-	$('.deal-button').prop('disabled', TF);
-	$('.hit-button').prop('disabled', TF);
-	$('.stand-button').prop('disabled', TF);
+////////////////////////////////////////////
+function disableButtons(bool){
+	$('.deal-button').prop('disabled', bool);
+	$('.hit-button').prop('disabled', bool);
+	$('.stand-button').prop('disabled', bool);
+}
+////////////////////////////////////////////
+
+function payout(playerOutcome, betAmount){
+	if (playerOutcome == 'blackjack'){
+		bank += betAmount + (betAmount * 1.5);
+	}else if(playerOutcome == 'win'){
+		bank += (betAmount * 2);
+	}else if(playerOutcome == 'push'){
+		bank += betAmount;
+	}
+	$('.bank-total').text(bank); // update html of bank
+	$('.bet-total').text('0'); // reset number in bet total html
+	bet = 0;
 }
